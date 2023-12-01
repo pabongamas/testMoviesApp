b9618e09bf86f3357cc4650bc55695a44462b801
pabongamas <72658365+pabongamas@users.noreply.github.com>
2023-12-01 00:12:23 -0500

    ajustes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,4 +8,14 @@ import { Movie,dataMovies } from 'src/app/models/movie.model';
 })
 export class HomeComponent {
   moviesData: Movie[] = dataMovies
+  typesSort:any=[
+    {id:1,sort:'Ranking'},
+    {id:2,sort:'Title'},
+    {id:3,sort:'Release Date'}
+  ];
+  typesSortSelected:number=1;
+  sort(event:Event|any){
+    let optionSelect=event.target.value;
+    console.log(optionSelect);
+  }
 }
